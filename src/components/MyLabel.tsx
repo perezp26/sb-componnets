ea8f7993184d4086c0f021fefa6dc5e09a88e8cf
raul perez <perezp@hotmail.com>
2022-02-28 13:26:11 -0600

    primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,39 @@
+import './mylabel.css'
+
+export interface MyLabelProps {
+     /**
+        * Este es el msg que se va a mostar en el Label
+    */
+    label :  string,
+
+     /**
+        * Esta son las opciones del tamaño del texto
+    */
+    size?: 'normal' | 'h1' | 'h2' | 'h3',
+    allCaps?: boolean,
+    color?: 'primary' | 'secondary' | 'tertiary',
+    fontColor?: string
+
+}
+
+
+export const MyLabel = ({
+    label   = 'Aqui va el texto',
+    size    = 'normal',
+    allCaps = false,
+    color   = 'primary',
+    fontColor
+} : MyLabelProps) => {
+  return (
+
+        <span 
+            className={ `label ${ size } ${'text-' + color}` }
+            style = {{ color: fontColor }}
+        >
+            { !allCaps ? label : label.toUpperCase() }
+        </span>
+
+  )
+}
+
+
